22afaafabe411cf8f57faef76ff77d0fdb86c22c
Isachenko Ivan <vanisachenko@gmail.com>
2023-09-29 18:46:11 +0700

    UserRepo fixes

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { userProviders } from './user/user.repository';
 import { databaseProviders } from './database.providers';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
@@ -15,8 +11,8 @@ import { UserModule } from './user/user.module';
               secret: jwtConstants.secret,
               signOptions: { expiresIn: '60d' },
             }),
-            UserModule,],
-  controllers: [UserController],
-  providers: [UserService, ...userProviders, ...databaseProviders],
+            ],
+  controllers: [],
+  providers: [...databaseProviders],
 })
 export class AppModule {}
