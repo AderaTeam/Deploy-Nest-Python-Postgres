b226830fb2cbdaf145286cfc165ea310be0f4c73
Isachenko Ivan <vanisachenko@gmail.com>
2023-09-29 20:19:40 +0700

    CORS FIXES

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -4,6 +4,12 @@ import "reflect-metadata"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:[
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ]
+  })
   await app.listen(8000);
 }
 bootstrap();
