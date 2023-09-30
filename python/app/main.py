59c00e30f24ad4b7f0df424c529ae61265158f40
Isachenko Ivan <vanisachenko@gmail.com>
2023-10-01 02:18:06 +0700

    debug

--- python/app/main.py
+++ python/app/main.py
@@ -80,6 +80,7 @@ def analyze_basic(id):
     cbc_wo_pensia_load.load_model('Models/classificator_catboost_wo_pensia.pkl')
     data = pd.read_csv('data/all_in_one_small.csv')
     data = data.loc[data.loc[:, "clnt_id"] == id]
+    print(data)
     gdp = pd.read_csv('data/gdp_processed.csv')
     userdata = mod_user_for_predict (data, gdp, space={'month': 4, 'year': 1}, classificator=cbc_wo_pensia_load, create_vector_user=create_vector_user, time_aproximator = scipy.signal.resample)
     return userdata
