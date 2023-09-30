3c3e0e2e639e1671d5aa42681f113e50a3ea9f1a
Isachenko Ivan <vanisachenko@gmail.com>
2023-09-30 13:10:03 +0700

    uvicorn fixes

--- python/app/main.py
+++ python/app/main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 app.add_middleware(
@@ -25,3 +26,6 @@ def analyze_mass():
     return "Placeholder for mass analysis"
 
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=80)
