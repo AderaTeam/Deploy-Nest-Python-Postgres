191711f826946671b60462e289c6560c1400b0a8
Isachenko Ivan <vanisachenko@gmail.com>
2023-09-30 11:52:38 +0700

    Crossapi routes start

--- nest-app/src/analysis/analysis.controller.ts
+++ nest-app/src/analysis/analysis.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AnalysisService } from './analysis.service';
 
 @Controller('analysis')
 export class AnalysisController {
+    constructor(
+        private readonly analysisService: AnalysisService
+    ){}
 
     @Get(':id')
-    public async personalAnalysis()
+    public async personalAnalysis(@Param('id') personId: number)
     {
+        return this.analysisService.analyzeSingle(personId);
+    }
 
+    @Get()
+    public async massAnalysis()
+    {
+        return this.analysisService.analyzeMass()
     }
 }
--- nest-app/src/analysis/analysis.service.ts
+++ nest-app/src/analysis/analysis.service.ts
@@ -1,4 +1,19 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class AnalysisService {}
+export class AnalysisService {
+    constructor(
+        private readonly httpService: HttpService
+    ){}
+
+    public analyzeSingle(id: number): Observable<any>
+    {
+        return this.httpService.get(process.env.ANALYZE_URL + `/${id}`)
+    }
+    public analyzeMass(): Observable<any>
+    {
+        return this.httpService.get(process.env.ANALYZE_URL + `/`)
+    }
+}
